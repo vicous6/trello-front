33f6b53e6d4cbe4a43f2598f9ae617565dbb5ba9
unknown <vicous@free.fr>
2024-03-14 20:52:46 +0200

    push

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect } from "react";
 import TrelloOAuth from "../models/trelloAuth.js";
 import Logo from "../assets/logo.png";
-import {redirect} from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 function Login() {
-  const trelloAuth = new TrelloOAuth("http://localhost:5173/main");
+  const trelloAuth = new TrelloOAuth(
+    "https://trello-front-ry4y.vercel.app:5173/main"
+  );
 
   const loginWithTrello = () => {
     const authUrl = trelloAuth.getAuthorizationUrl();
     window.location.href = authUrl;
   };
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        console.log(token)
-        if(token && token !== "undefined"){
-            window.location.href = "/main";
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    if (token && token !== "undefined") {
+      window.location.href = "/main";
+    }
+  }, []);
 
   return (
-      <div id="loginPage">
-          <img src={Logo} alt="NOTED logo"/>
-          <div>
-                  <button onClick={loginWithTrello}>Se connecter avec Trello</button>
-          </div>
+    <div id="loginPage">
+      <img src={Logo} alt="NOTED logo" />
+      <div>
+        <button onClick={loginWithTrello}>Se connecter avec Trello</button>
       </div>
+    </div>
   );
 }
 
